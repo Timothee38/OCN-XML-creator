06f87aebadfcc0814a590b95ea2d2c56d85ae1ee
Elliott <elliottwhyman@hotmail.co.uk>
2014-05-10 14:52:38 +0100

    Update start.java

--- OCNXMLGenerator/src/io/ocnxmlgen/start.java
+++ OCNXMLGenerator/src/io/ocnxmlgen/start.java
@@ -25,7 +25,6 @@ public class start extends JFrame {
 	
 	public start() {
 		super("Overcast Network XML generator");
-		//below can be ignored - auto generated for positioning 
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{1, 386, 0, 0};
 		gridBagLayout.rowHeights = new int[]{14, 0, 23, 0};
@@ -34,7 +33,8 @@ public class start extends JFrame {
 		getContentPane().setLayout(gridBagLayout);
 				
 
-						//below can be ignored - auto generated for positioning 
+
+					
 						welcomemsg = new JLabel("Welcome to the unofficial OCN XML generator by Captain_Elliott and Timothee38");
 						GridBagConstraints gbc_welcomemsg = new GridBagConstraints();
 						gbc_welcomemsg.anchor = GridBagConstraints.NORTHWEST;
@@ -47,7 +47,6 @@ public class start extends JFrame {
 				
 
 				startbtn = new JButton("Start");
-				//below can be ignored - auto generated for positioning 
 				GridBagConstraints gbc_startbtn = new GridBagConstraints();
 				gbc_startbtn.insets = new Insets(0, 0, 5, 0);
 				gbc_startbtn.anchor = GridBagConstraints.NORTH;
@@ -56,7 +55,6 @@ public class start extends JFrame {
 				getContentPane().add(startbtn, gbc_startbtn);
 				
 				FileDir = new JLabel("File Path: ");
-				//below can be ignored - auto generated for positioning 
 				GridBagConstraints gbc_lblFilePath = new GridBagConstraints();
 				gbc_lblFilePath.insets = new Insets(0, 0, 5, 5);
 				gbc_lblFilePath.gridx = 1;
@@ -65,7 +63,6 @@ public class start extends JFrame {
 				FileDir.setVisible(false);
 				
 				NextProtobtn = new JButton("Next : Proto");
-				//below can be ignored - auto generated for positioning 
 				GridBagConstraints gbc_btnNextProto = new GridBagConstraints();
 				gbc_btnNextProto.insets = new Insets(0, 0, 5, 0);
 				gbc_btnNextProto.gridx = 2;
@@ -74,14 +71,12 @@ public class start extends JFrame {
 				NextProtobtn.setVisible(false);
 				
 				mapProtoValue = new JLabel();
-				//below can be ignored - auto generated for positioning 
 				GridBagConstraints gbc_lblMapProto = new GridBagConstraints();
 				gbc_lblMapProto.insets = new Insets(0, 0, 0, 5);
 				gbc_lblMapProto.gridx = 1;
 				gbc_lblMapProto.gridy = 2;
 				getContentPane().add(mapProtoValue, gbc_lblMapProto);
 				mapProtoValue.setVisible(false);
-				
 				
 				startbtn.addActionListener(new ActionListener() { //action listener to check for a button press event
 					public void actionPerformed(ActionEvent arg02) { //on action performed do...
